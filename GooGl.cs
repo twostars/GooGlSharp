020bd64eb0cb06277c3e7f9da730b7c045b7d4c5
twostars <i.am.twostars@gmail.com>
2013-07-09 20:08:55 +1000

    Initial commit

---
+++ GooGl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GooGlSharp
+{
+	public class GooGl
+	{
+        private readonly GooGlService _gooGlService;
+        private readonly Dictionary<string, Uri> _urlCache;
+
+        public GooGl()
+        {
+            _gooGlService = new GooGlService();
+            _urlCache = new Dictionary<string, Uri>();
+        }
+
+        private Uri AttemptLookup(string url, string apiKey)
+        {
+            var key = url;
+            if (_urlCache.ContainsKey(key))
+                return _urlCache[key];
+
+            try
+            {
+                var shortUri = _gooGlService.Execute(url, apiKey);
+                _urlCache.Add(key, shortUri);
+                return shortUri;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Uri Shorten(Uri url, string apiKey)
+        {
+            return AttemptLookup(url.ToString(), apiKey);
+        }
+	}
+}
---
+++ GooGlService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace GooGlSharp
+{
+	public class GooGlService
+	{
+        private const int RequestTimeout = 30 * 1000; // 30 seconds
+
+        public Uri Execute(string url, string key)
+        {
+            var request = HttpWebRequest.Create(string.Format("https://www.googleapis.com/urlshortener/v1/url?key={0}", key)) as HttpWebRequest;
+
+            request.UserAgent = "GooGlSharp";
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            request.Timeout = RequestTimeout;
+
+            // Shouldn't need a full Json formatter for this.
+            var json = "{\"longUrl\": \"" + url + "\"}";
+            var bytes = Encoding.ASCII.GetBytes(json);
+            request.ContentLength = bytes.Length;
+
+            using (var os = request.GetRequestStream())
+                os.Write(bytes, 0, bytes.Length);
+
+            using (var response = request.GetResponse() as HttpWebResponse)
+            {
+                using (var responseStream = response.GetResponseStream())
+                using (var responseReader = new StreamReader(responseStream))
+                {
+                    json = responseReader.ReadToEnd();
+                    return new Uri(Regex.Match(json, @"""id"": ?""(?<id>.+)""").Groups["id"].Value);
+                }
+            }
+        }
+	}
+}
